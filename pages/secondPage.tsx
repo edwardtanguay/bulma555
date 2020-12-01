ee6c86a9264522e61594a8a041bf8e5604f08b57
Edward Tanguay (wk) <edward@tanguay.info>
2020-12-01 17:37:54 +0100

    small change

--- pages/secondPage.tsx
+++ pages/secondPage.tsx
@@ -1,12 +1,13 @@
 import Layout from "../components/layout";
 
 const SecondPage = () => (
-  <Layout>
-    <section className="section">
-      <div className="container">
-        <h1 className="title">Second page</h1>
-      </div>
-    </section>
-  </Layout>
+	<Layout>
+		<section className="section">
+			<div className="container">
+				<h1 className="title">Second page</h1>
+				<h2 className="subtitle is-4">This is the subtitle22</h2>
+			</div>
+		</section>
+	</Layout>
 );
 export default SecondPage;
